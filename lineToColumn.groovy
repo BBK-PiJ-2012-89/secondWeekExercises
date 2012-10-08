a90a81e2c144a2e8ab4d05a70aeb746c2814e12e
charliembrodie <cbrodi01@cspc053.dcs.bbk.ac.uk>
2012-10-08 14:41:33 +0100

    I completed this exercise very quickly with no need for more than one commit, full functionality, working while and for loops

---
+++ lineToColumn.groovy
@@ -0,0 +1,16 @@
+print "Please write in some text of your choosing: "
+String text = System.console().readLine()
+
+int b = text.length()
+
+for(int a = 0; a != b; a++){
+   println text.charAt(a)
+}
+
+int a = 0
+
+/*
+while(a != b){
+ println text.charAt(a)
+ a++
+}*/
