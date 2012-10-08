8fedc56619761a9e5a626f4f3d2d5847ba731def
charliembrodie <cbrodi01@cspc053.dcs.bbk.ac.uk>
2012-10-08 14:56:22 +0100

    I have fulfilled the second two demands of the question, there is now full letter selection and counting functionality, exercise complete

--- countingLetters.groovy
+++ countingLetters.groovy
@@ -1,11 +1,16 @@
 print "Type whatever you want: "
 String text = System.console().readLine()
 
+print "Thank you, now please enter a letter for me to count: "
+String letter = System.console().readLine()
+
 int eCounter
 for(a = 0; a != text.length(); a++){
-    if(text.charAt(a)== "e"){
+    if(text.charAt(a)== letter){
 	eCounter++
 	}
 }
-print "There are " + eCounter
-print " e's in the above text."
+
+print "There are " + eCounter 
+print " " + letter
+print "'s in the above text"
