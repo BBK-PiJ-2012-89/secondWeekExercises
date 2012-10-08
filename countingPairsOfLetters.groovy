3ef2e65387c82c59fb3734f9cb19ee9acc58b022
charliembrodie <cbrodi01@cspc053.dcs.bbk.ac.uk>
2012-10-08 15:07:16 +0100

    I have laid the majority of the framework, but have yet to test for functionality

---
+++ countingPairsOfLetters.groovy
@@ -0,0 +1,17 @@
+print "Enter a pair of letters: "
+String pair = System.console().readLine()
+
+print "now please enter a long block of text: "
+String text = System.console().readLine()
+
+int pairCounter
+
+for(a = 0; a != text.length(); a++){
+   if(text.charAt(a) == pair.charAt(0) && text.charAt(a+1) == pair.charAt(1){
+      pairCounter++
+	 }
+}
+
+print "The pair of letters " + pair
+print " appears " +pairCounter
+print " times in the text."
