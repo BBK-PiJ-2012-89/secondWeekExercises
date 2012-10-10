b72a55bade7cb7948b2021b81cb8940d889f5801
charliembrodie <cbrodi01@cspc053.dcs.bbk.ac.uk>
2012-10-10 13:00:03 +0100

    I have finished exercise 10 after working out how to use the new methods, exercise complete.

--- palindromeRedux.groovy
+++ palindromeRedux.groovy
@@ -2,17 +2,24 @@ print "Enter a word: "
 String pal = System.console().readLine()
 int len = pal.length()
 
+String pal2 = ""
 String backwards = ""
 
 for(int a = len-1; a >= 0; a--){
     if(Character.isLetter(pal.charAt(a)) == true){
-	    Character.toLowerCase(pal.charAt(a)) 
-        backwards = backwards + pal.charAt(a)
+        backwards = backwards + Character.toLowerCase(pal.charAt(a))
 	}
 }
 println backwards
 
-if(backwards == pal){
+for(int a = 0; a < pal.length(); a++){
+    if(Character.isLetter(pal.charAt(a)) == true){
+	    pal2 = pal2 + Character.toLowerCase(pal.charAt(a)) 
+	}
+}
+println pal2
+
+if(backwards == pal2){
     print "What you have there is a relaxed palindrome"
 } else {
     print "No relaxed palindrome for you"
